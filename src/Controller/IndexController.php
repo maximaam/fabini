ed0e145a63031e7e82d70f3d92070ad0d00aa428
Maamar Khemissi <maamar.khemissi@meingutscheincode.com>
2018-02-16 18:26:33 +0100

    symfony 4

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Leather;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,15 +10,69 @@ use Symfony\Component\HttpFoundation\Response;
 class IndexController extends Controller
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="home")
      */
     public function index()
     {
-
-        return $this->render('app/index.html.twig');
-
+        $leather = $this->getDoctrine()->getRepository(Leather::class)->findBy([], [], 3);
 
 
-        //return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
+        return $this->render('app/index.html.twig', [
+            'leather' => $leather
+        ]);
+    }
+
+    /**
+     * @Route("/{_locale}/ueber-us", name="about-us_de")
+     * @Route("/{_locale}/about-us", name="about-us_en")
+     */
+    public function aboutUs()
+    {
+        return $this->render('app/about-us.html.twig');
+    }
+
+    /**
+     * @Route("/{_locale}/impressum", name="imprint_de")
+     * @Route("/{_locale}/imprint", name="imprint_en")
+     */
+    public function imprint()
+    {
+        return $this->render('app/imprint.html.twig');
+    }
+
+    /**
+     * @Route("/{_locale}/kontakt", name="contact_de")
+     * @Route("/{_locale}/contact", name="contact_en")
+     */
+    public function contact()
+    {
+        return $this->render('app/contact.html.twig');
+    }
+
+    /**
+     * @Route("/{_locale}/lederboersen", name="leather-wallets_de")
+     * @Route("/{_locale}/leather-wallets", name="leather-wallets_en")
+     */
+    public function leatherWallets()
+    {
+        return $this->render('app/leather-wallets.html.twig');
+    }
+
+    /**
+     * @Route("/{_locale}/ledertaschen", name="leather-bags_de")
+     * @Route("/{_locale}/leather-bags", name="leather-bags_en")
+     */
+    public function leatherBags()
+    {
+        return $this->render('app/leather-bags.html.twig');
+    }
+
+    /**
+     * @Route("/{_locale}/Lederhandschuhe", name="leather-gloves_de")
+     * @Route("/{_locale}/leather-gloves", name="leather-gloves_en")
+     */
+    public function leatherGloves()
+    {
+        return $this->render('app/leather-gloves.html.twig');
     }
 }
---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mimosa
+ * Date: 15.02.18
+ * Time: 16:13
+ */
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MenuBuilder
+{
+
+    private $factory;
+
+    /**
+     * @var RequestStack
+     */
+    private $request;
+
+    /**
+     * MenuBuilder constructor.
+     *
+     * @param FactoryInterface $factory
+     * @param RequestStack $request
+     */
+    public function __construct(FactoryInterface $factory, RequestStack $request)
+    {
+        $this->factory = $factory;
+        $this->request = $request;
+    }
+
+    /**
+     * @param array $options
+     * @return \Knp\Menu\ItemInterface
+     */
+    public function createMainMenu(array $options)
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'navbar-nav mr-auto');
+
+        $locale = $this->request->getCurrentRequest()->getLocale();
+
+        $items = [
+            'de' => [
+                ['label' => 'LEDERBÖRSEN', 'route' => 'leather-wallets_de'],
+                ['label' => 'LEDERTASCHEN', 'route' => 'leather-bags_de'],
+                ['label' => 'LEDERHANDSCHUHE', 'route' => 'leather-gloves_de'],
+            ],
+            'en' => [
+                ['label' => 'LEDERBÖRSEN', 'route' => 'leather-wallets_de'],
+                ['label' => 'LEDERTASCHEN', 'route' => 'leather-bags_de'],
+                ['label' => 'LEDERHANDSCHUHE', 'route' => 'leather-gloves_de'],
+            ]
+        ];
+
+        foreach ($items[$locale] as $key => $val) {
+            $menu->addChild($val['label'], [
+                'route' => $val['route'],
+                'attributes' => ['class' => 'nav-item'],
+                'linkAttributes' => ['class' => 'nav-link']
+            ]);
+        }
+
+        //dump($menu);
+
+        //die;
+
+
+        return $menu;
+    }
+
+    /**
+     * @param array $options
+     * @return \Knp\Menu\ItemInterface
+     */
+    public function createFooterMenu(array $options)
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'footer-nav');
+
+        $locale = $this->request->getCurrentRequest()->getLocale();
+
+        $items = [
+            'de' => [
+                ['label' => 'Über uns', 'route' => 'about-us_de'],
+                ['label' => 'Impressum', 'route' => 'imprint_de'],
+                ['label' => 'Contact', 'route' => 'contact_de'],
+            ],
+            'en' => [
+                ['label' => 'About us', 'route' => 'about-us_en'],
+                ['label' => 'Imprint', 'route' => 'imprint_en'],
+                ['label' => 'Contact', 'route' => 'contact_en'],
+            ]
+        ];
+
+        foreach ($items[$locale] as $key => $val) {
+            $menu->addChild($val['label'], ['route' => $val['route']]);
+        }
+
+        return $menu;
+    }
+}
